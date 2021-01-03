1c80bc0239e6150c9a415f37a79d1a4f82d8da36
Steven Creighton <ste.creighton@googlemail.com>
2021-01-03 16:03:27 +0000

    first commit

---
+++ backend/server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import colors from "colors";
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
+);
