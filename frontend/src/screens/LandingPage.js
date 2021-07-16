de12672b4c1a6acc8afb3b53cfcb9ccdc6960974
Steven Creighton <ste.creighton@googlemail.com>
2021-07-16 17:31:47 +0100

    16/07/2021

--- frontend/src/screens/LandingPage.js
+++ frontend/src/screens/LandingPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/landingPage.css";
 import { useDispatch, useSelector } from "react-redux";
 import { LinkContainer } from "react-router-bootstrap";
@@ -6,6 +6,8 @@ import { toggleMenuActive } from "../actions/landingActions";
 
 const LandingPage = () => {
   const dispatch = useDispatch();
+
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
 
   const menuToggle = useSelector((state) => state.menuToggle);
   const { menuActive } = menuToggle;
@@ -27,7 +29,7 @@ const LandingPage = () => {
   return (
     <>
       <section className={`showcase ${menuActive}`}>
-        <header>
+        <header className={`${menuActive === "active" ? "d-none" : ""}`}>
           <h2 className={`logo ${menuActive}`}>The Kebabery</h2>
         </header>
 
@@ -38,63 +40,93 @@ const LandingPage = () => {
           autoplay=""
           className={`${menuActive}`}
         ></video>
-        <div className="overlay"></div>
-        <div className={`text ${menuActive}`}>
-          <h2>The finest kebabs</h2>
-          <h3>in all of Kebabdom</h3>
-          <p>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam
-            tempora laboriosam, voluptatibus maxime beatae corporis quae
-            molestiae recusandae ea quas!
-          </p>
-        </div>
+        <div className="container">
+          <div className="overlay"></div>
+          <div className={`text ${menuActive}`}>
+            <h2>The finest kebabs</h2>
+            <h3>in all of Kebabdom</h3>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam
+              tempora laboriosam, voluptatibus maxime beatae corporis quae
+              molestiae recusandae ea quas!
+            </p>
+          </div>
 
-        <footer>
-          <h2
-            className={`bottom-logo ${menuActive}`}
-            onClick={() => toggleHandler("logo")}
+          <div
+            className={`row footer ${
+              menuActive === "active" ? " " : "mt-auto"
+            }`}
           >
-            The Kebabery
-          </h2>
-          <LinkContainer to="/">
-            <div className="menu-item" onClick={() => toggleHandler("menu")}>
-              Menu
+            <h2
+              className={`bottom-logo ${menuActive}`}
+              onClick={() => toggleHandler("logo")}
+            >
+              The Kebabery
+            </h2>
+
+            <div
+              className="menu-item mx-auto text d-block d-md-none"
+              onClick={() => {
+                toggleHandler("menu");
+                setShowMobileMenu(!showMobileMenu);
+              }}
+            >
+              <i className="fas fa-bars"></i>
             </div>
-          </LinkContainer>
-          <LinkContainer to="/contacts" className="d-none d-md-block">
-            <div className="menu-item" onClick={() => toggleHandler("menu")}>
-              Contact Us
-            </div>
-          </LinkContainer>
-          <LinkContainer to="/basket" className="d-none d-md-block">
-            <div className="menu-item" onClick={() => toggleHandler("menu")}>
-              Your Order{" "}
-              {basketItems.length > 0 ? `(${basketItems.length})` : ""}
-            </div>
-          </LinkContainer>
-          {userInfo ? (
-            <LinkContainer to="/profile" className="d-none d-md-block">
+
+            <LinkContainer to="/" className="d-none d-md-block text">
               <div className="menu-item" onClick={() => toggleHandler("menu")}>
-                Profile
+                Menu
               </div>
             </LinkContainer>
-          ) : (
-            <LinkContainer to="/login" className="d-none d-md-block">
+            <LinkContainer to="/contacts" className="d-none d-md-block text">
               <div className="menu-item" onClick={() => toggleHandler("menu")}>
-                Login
+                Contact Us
               </div>
             </LinkContainer>
-          )}
-          {userInfo && userInfo.isAdmin ? (
-            <LinkContainer to="/admin/orders" className="d-none d-md-block">
+            <LinkContainer to="/basket" className="d-none d-md-block text">
               <div className="menu-item" onClick={() => toggleHandler("menu")}>
-                Admin Options
+                Your Order{" "}
+                {basketItems.length > 0 ? `(${basketItems.length})` : ""}
               </div>
             </LinkContainer>
-          ) : (
-            ""
-          )}
-        </footer>
+            {userInfo ? (
+              <LinkContainer to="/profile" className="d-none d-md-block text">
+                <div
+                  className="menu-item"
+                  onClick={() => toggleHandler("menu")}
+                >
+                  Profile
+                </div>
+              </LinkContainer>
+            ) : (
+              <LinkContainer to="/login" className="d-none d-md-block text">
+                <div
+                  className="menu-item"
+                  onClick={() => toggleHandler("menu")}
+                >
+                  Login
+                </div>
+              </LinkContainer>
+            )}
+            {userInfo && userInfo.isAdmin ? (
+              <LinkContainer
+                to="/admin/orders"
+                className="d-none d-md-block text"
+              >
+                <div
+                  className="menu-item"
+                  onClick={() => toggleHandler("menu")}
+                >
+                  Admin Options
+                </div>
+              </LinkContainer>
+            ) : (
+              ""
+            )}
+          </div>
+          {showMobileMenu ? <div className="row">Menu</div> : ""}
+        </div>
       </section>
     </>
   );
